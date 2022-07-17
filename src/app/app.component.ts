344b82c7a7bbe7438b1d0b5293efd6149cbb898d
marshalln@dotcomsoftware.co.za <marshalln@dotcomsoftware.co.za>
2022-07-17 11:32:22 +0200

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { environment } from 'src/environments/environment';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  title = 'azure-pipeline-app';
+  client_env_name: string = environment.env_name;
+  server_env_name: string = '';
+}
